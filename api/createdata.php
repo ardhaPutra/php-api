06538697d28c13538a947d9d7f250d19418e1622
ardha78 <ardhakrs@gmail.com>
2023-03-01 17:23:37 +0800

    refactor createdata

--- api/createdata.php
+++ api/createdata.php
@@ -10,9 +10,12 @@ header("Access-Control-Allow-Credentials", "true");
 require '../config/app.php';
 
 // menerima input
-$nama   = $_POST['nama'];
-$jumlah = $_POST['jumlah'];
-$harga  = $_POST['harga'];
+// $nama   = $_POST['nama'];
+// $jumlah = $_POST['jumlah'];
+// $harga  = $_POST['harga'];
+
+// Terima data dari Axios
+$data = json_decode(file_get_contents('php://input'), true);
 
 // validasi data
 if ($nama == null) {
@@ -21,9 +24,9 @@ if ($nama == null) {
 } 
 
 // query tambah data
-$query = "INSERT INTO barang (nama,jumlah,harga) VALUES ('$nama', '$jumlah', '$harga')";
+// $query = "INSERT INTO barang (nama,jumlah,harga) VALUES ('$nama', '$jumlah', '$harga')";
+$query = "INSERT INTO barang (nama, jumlah, harga) VALUES ('".$data['nama']."', '".$data['jumlah']."', '".$data['alamat']."', '".$data['harga']."')";
 $hasil=mysqli_query($db, $query);
-
 if ($hasil) {
     echo json_encode(['success' => true, 'message' => 'Data berhasil ditambahkan.']);
 } else {    
