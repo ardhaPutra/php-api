db3cf803744922092d706bf3ae3e1db44533d18c
ardha78 <ardhakrs@gmail.com>
2023-03-02 12:30:51 +0800

    update createdata and delete

--- api/createdata.php
+++ api/createdata.php
@@ -3,30 +3,31 @@
 // render halaman menjadi json
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 header("Access-Control-Allow-Credentials", "true");
 
 require '../config/app.php';
 
-// menerima input
-// $nama   = $_POST['nama'];
-// $jumlah = $_POST['jumlah'];
-// $harga  = $_POST['harga'];
+// menerima input dari request POST
+$inputJSON = file_get_contents('php://input');
+$input = json_decode($inputJSON, TRUE);
 
-// Terima data dari Axios
-$data = json_decode(file_get_contents('php://input'), true);
+// memperoleh data dari input
+$nama   = $input['nama'];
+$jumlah = $input['jumlah'];
+$harga  = $input['harga'];
 
 // validasi data
-if ($nama == null) {
+if (empty($nama)) {
     echo json_encode(['pesan' => 'Nama barang Tidak Boleh Kosong']);
     exit;
 } 
 
 // query tambah data
-// $query = "INSERT INTO barang (nama,jumlah,harga) VALUES ('$nama', '$jumlah', '$harga')";
-$query = "INSERT INTO barang (nama, jumlah, harga) VALUES ('".$data['nama']."', '".$data['jumlah']."', '".$data['alamat']."', '".$data['harga']."')";
-$hasil=mysqli_query($db, $query);
+$query = "INSERT INTO barang (nama,jumlah,harga) VALUES ('$nama', '$jumlah', '$harga')";
+$hasil = mysqli_query($db, $query);
+
 if ($hasil) {
     echo json_encode(['success' => true, 'message' => 'Data berhasil ditambahkan.']);
 } else {    
--- api/delete.php
+++ api/delete.php
@@ -1,32 +1,28 @@
 <?php
 
-// render halaman menjadi json
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header('Access-Control-Allow-Methods: DELETE');
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Referrer-Policy: no-referrer-when-downgrade");
 
 require '../config/app.php';
 
-// menerima request put/delete
 parse_str(file_get_contents('php://input'), $delete);
-$id_barang = $input['id_barang'] ?? null;
+// mengganti $_GET dengan $delete
+$id_barang = $delete['id_barang'] ?? null;
 
 if (!$id_barang) {
-    die('id_barang tidak ditemukan');
+  die('id_barang tidak ditemukan');
 }
 
-$id_barang = $delete['id_barang'];
-
-// query hapus data
 $query  = "DELETE FROM barang WHERE id_barang = $id_barang";
 $hasil = mysqli_query($db, $query);
 
-// check status data
 if ($hasil) {
-    echo json_encode(['pesan' => 'Data barang Berhasil Dihapus']);
+  echo json_encode(['pesan' => 'Data barang berhasil dihapus']);
 } else {    
-    echo json_encode(['pesan' => 'Data barang Gagal Dihapus  ']);
+  echo json_encode(['pesan' => 'Data barang gagal dihapus']);
 }
